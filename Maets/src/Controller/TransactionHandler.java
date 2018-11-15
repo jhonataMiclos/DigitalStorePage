990b46391113ea121c0edbcf0255aafd930cfef2
Colum Mullally <15142434@studentmail.ul.ie>
2018-11-15 21:14:25 +0000

    clean

--- Maets/src/Controller/TransactionHandler.java
+++ Maets/src/Controller/TransactionHandler.java
@@ -22,18 +22,7 @@ public class TransactionHandler {
         rA =  rf.getRepoAccess();
     }
     
-    public boolean validateCard(String cardNumber) {
-        String pattern = "^(?:4[0-9]{12}(?:[0-9]{3})?" +
-" |  (?:5[1-5][0-9]{2}" +
-"     | 222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}" +
-" |  3[47][0-9]{13}" +
-" |  3(?:0[0-5]|[68][0-9])[0-9]{11}" +
-" |  6(?:011|5[0-9]{2})[0-9]{12}" +
-" |  (?:2131|1800|35\\d{3})\\d{11}" +
-")$";
-        return true;
-       // return cardNumber.matches(pattern);
-    }
+
     public boolean AddToLibrary() {
        uiHandler.pullDownCart();
        JSONArray cart = uiHandler.cart;
